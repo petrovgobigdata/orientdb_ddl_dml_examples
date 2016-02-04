2b092858eb8b03dda0b37ea3e2d5d7837c48201f
petrovgobigdata <petrov.y.v@gmail.com>
2016-02-04 18:56:11 +0300

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -1,1 +1,15 @@
+--Создаем документный класс (таблицу)
+CREATE CLASS words_doc
 
+--Изменим INSERTы из Aqua Data Studio
+--Search
+--;
+--INSERT INTO words_doc(wordid, lemma)
+--  VALUES
+--Replace
+--,
+--------------------
+--Спецсимволы экранируем!
+--например '' заменяем на \'
+INSERT INTO words_doc(wordid, lemma)
+VALUES(405, 'ab'), (406, 'ab initio'), (407, 'aba'), (408, 'aba transit number'), (409, 'abaca')
