5e91332425dd84045dbbae7f85c1d668636dd210
petrovgobigdata <petrov.y.v@gmail.com>
2016-02-04 20:20:22 +0300

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -16,5 +16,11 @@ INSERT INTO words_doc(wordid, lemma)
 VALUES(405, 'ab'), (406, 'ab\'s initio'), (407, 'aba'), (408, 'aba transit number'), (409, 'abaca')
 
 --Создаем графовый класс и вершину
+CREATE CLASS V
+CREATE CLASS E
 CREATE CLASS words_graph EXTENDS V
+
 CREATE VERTEX words_graph
+INSERT INTO words_graph FROM SELECT FROM words_doc
+
+DELETE VERTEX words_graph
