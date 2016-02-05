cdee7d1d970f29d191e5e690697344678c78e48e
petrovgobigdata <petrov.y.v@gmail.com>
2016-02-05 12:45:50 +0300

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -19,8 +19,11 @@ VALUES(405, 'ab'), (406, 'ab\'s initio'), (407, 'aba'), (408, 'aba transit numbe
 CREATE CLASS V
 CREATE CLASS E
 CREATE CLASS words_v EXTENDS V
+CREATE CLASS has_syn_e EXTENDS E
 
 CREATE VERTEX words_v
+CREATE EDGE syn_e
+
 INSERT INTO words_v FROM SELECT FROM words_doc
 
 DELETE VERTEX words_graph BATCH 1000
