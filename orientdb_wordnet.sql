b284b904b507472eec9801be9e881b90d5443b6e
petrovgobigdata <petrov.y.v@gmail.com>
2016-02-10 01:24:18 +0400

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -8,9 +8,15 @@ CREATE CLASS has_syn_e EXTENDS E
 --INSERT INTO words_v FROM SELECT FROM words_doc
 
 SELECT FROM words_v
-SELECT EXPAND( OUT() ) FROM words_v where wordid = 405
+SELECT FROM words_v where wordid = 405
 --Отобрать только те записи у которых есть исходящие ребра (есть синонимы)
-SELECT EXPAND( BOTH( 'has_syn_e' ) ) FROM words_v where outE().size() > 0 limit 100
+SELECT FROM words_v where outE().size() > 0 limit 100
+--Отобрать только те слова у которых есть синонимы но которые не являются синонимом ни для кого
+SELECT FROM words_v where outE().size() > 0 and inE().size() = 0 limit 100
+--Отобрать только те записи у которых есть > 10 синонимов
+SELECT *, OUT().lemma FROM words_v where outE().size() > 10 limit 100
+--Отобрать только те записи у которых есть > 10 синонимов и транспонировать
+SELECT *, OUT().lemma as lemma_tr FROM words_v where outE().size() > 10 UNWIND lemma_tr limit 100
 
 DELETE EDGE has_syn_e LIMIT 1000000
 DELETE VERTEX words_v LIMIT 1000000
