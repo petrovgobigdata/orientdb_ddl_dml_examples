5d515d1bf140ba14fc88c20f96f68539a9b9c004
petrovgobigdata <petrov.y.v@gmail.com>
2016-02-11 13:44:19 +0300

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -20,6 +20,12 @@ SELECT *, OUT().lemma FROM words_v where outE().size() > 10 limit 100
 --Отобрать только те записи у которых есть > 10 синонимов и транспонировать
 SELECT *, OUT().lemma as lemma_tr FROM words_v where outE().size() > 10 UNWIND lemma_tr limit 100
 
+select wordid, IN('has_senses_e').lemma as lemma, synsetid, outE('has_link_e').linktype[0] as linktype, OUT('has_link_e').wordid as wordid_link
+from senses_v 
+where outE('has_link_e').linktype[0] = 'synonym' 
+order by wordid UNWIND lemma, wordid_link
+limit 1000
+
 DELETE EDGE has_senses_e LIMIT 1000000
 DELETE EDGE has_link_e LIMIT 1000000
 DELETE VERTEX words_v LIMIT 1000000
