7f824dccb23d686b3b46ca494be0352663842e8b
petrovgobigdata <petrov.y.v@gmail.com>
2016-04-04 01:05:20 +0300

    Update orientdb_document_oriented.sql

--- orientdb_document_oriented.sql
+++ orientdb_document_oriented.sql
@@ -1,3 +1,13 @@
+create class party
 
-
-INSERT INTO Employee CONTENT {name : 'Jay', surname : 'Miner', gender : 'M'}
+INSERT INTO party CONTENT {
+    "name": "Yury",
+    "sex": "M",
+    "birthday": "1985-01-15",
+    "addr": [
+        {"location": "Саратов", "street": "Новая", "home": "15", "date_from": "1985-01-15", "date_to": "1997-05-25"}
+        {"location": "Серпухов", "street": "Мира", "home": "29", "date_from": "1997-05-25", "date_to": "1998-07-19"}
+        {"location": "Подольск", "street": "Парковая", "home": "17", "date_from": "1998-07-19", "date_to": "2005-05-15"}
+        {"location": "Москва", "street": "Лесная", "home": "5", "date_from": "2005-05-15", "date_to": "3000-01-01"}
+    ]
+}
