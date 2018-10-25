8977c9d2c81e6c67eee69f1ac98c7ef58afea398
petrovgobigdata <petrov.y.v@gmail.com>
2018-10-25 10:09:48 +0300

    Update orientdb_wordnet.sql

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -27,6 +27,7 @@ CREATE PROPERTY senses_v.synsetid STRING
 CREATE PROPERTY senses_v.definition STRING
 CREATE PROPERTY senses_v.casedwordid STRING
 CREATE INDEX senses_v.nui_senses_v_synsetid_seq ON senses_v (synsetid_seq) NOTUNIQUE
+CREATE INDEX senses_v.nui_senses_v_synsetid ON senses_v (synsetid) NOTUNIQUE
 CREATE INDEX senses_v.nui_senses_v_wordid_seq ON senses_v (wordid_seq, synsetid_seq) NOTUNIQUE
 CREATE INDEX senses_v.nui_senses_v_wordid ON senses_v (wordid) NOTUNIQUE
 CREATE INDEX senses_v.nui_senses_v_casedwordid ON senses_v (casedwordid) NOTUNIQUE
