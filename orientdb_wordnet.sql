d249f73282fa63107f166f267adc253d2f4dd34a
petrovgobigdata <petrov.y.v@gmail.com>
2020-04-14 19:11:19 +0300

    CREATE UNIQUE INDEX
    CREATE UNIQUE INDEX on edges added

--- orientdb_wordnet.sql
+++ orientdb_wordnet.sql
@@ -6,58 +6,66 @@ CREATE CLASS V
 CREATE CLASS E
 
 --Создаем последовательность (для автоинкремента)
-CREATE SEQUENCE sqlunet_wordid TYPE ORDERED START 0 INCREMENT 1
-CREATE SEQUENCE sqlunet_synsetid TYPE ORDERED START 0 INCREMENT 1
+CREATE SEQUENCE sqlunet_wordid TYPE ORDERED START 0 INCREMENT 1;
+CREATE SEQUENCE sqlunet_synsetid TYPE ORDERED START 0 INCREMENT 1;
 CREATE SEQUENCE sqlunet_sample_seq TYPE ORDERED START 0 INCREMENT 1
 --Создаем вершины и ребра словаря
-CREATE CLASS words_v EXTENDS V
-CREATE PROPERTY words_v.wordid_seq LONG
-CREATE PROPERTY words_v.wordid STRING
-CREATE PROPERTY words_v.casedwordid STRING
-CREATE PROPERTY words_v.lemma STRING
-CREATE PROPERTY words_v.wordtype STRING
-CREATE PROPERTY words_v.origin STRING
-CREATE INDEX words_v.nui_words_v_lemma ON words_v (lemma) NOTUNIQUE
-CREATE INDEX words_v.nui_words_v_wordid ON words_v (wordid) NOTUNIQUE
+CREATE CLASS words_v EXTENDS V;
+CREATE PROPERTY words_v.wordid_seq LONG;
+CREATE PROPERTY words_v.wordid STRING;
+CREATE PROPERTY words_v.casedwordid STRING;
+CREATE PROPERTY words_v.lemma STRING;
+CREATE PROPERTY words_v.wordtype STRING;
+CREATE PROPERTY words_v.origin STRING;
+CREATE INDEX words_v.nui_words_v_lemma ON words_v (lemma) NOTUNIQUE;
+CREATE INDEX words_v.nui_words_v_wordid ON words_v (wordid) NOTUNIQUE;
 CREATE INDEX words_v.nui_words_v_wordid_seq ON words_v (wordid_seq) NOTUNIQUE
 
-CREATE CLASS senses_v EXTENDS V
-CREATE PROPERTY senses_v.synsetid_seq LONG
-CREATE PROPERTY senses_v.synsetid STRING
-CREATE PROPERTY senses_v.definition STRING
-CREATE INDEX senses_v.nui_senses_v_synsetid_seq ON senses_v (synsetid_seq) NOTUNIQUE
+CREATE CLASS senses_v EXTENDS V;
+CREATE PROPERTY senses_v.synsetid_seq LONG;
+CREATE PROPERTY senses_v.synsetid STRING;
+CREATE PROPERTY senses_v.definition STRING;
+CREATE INDEX senses_v.nui_senses_v_synsetid_seq ON senses_v (synsetid_seq) NOTUNIQUE;
 CREATE INDEX senses_v.nui_senses_v_synsetid ON senses_v (synsetid) NOTUNIQUE
 
-CREATE CLASS samples_v EXTENDS V
-CREATE PROPERTY samples_v.synsetid_seq LONG
-CREATE PROPERTY samples_v.sample_seq LONG
-CREATE PROPERTY samples_v.synsetid STRING
-CREATE PROPERTY samples_v.sample STRING
-CREATE INDEX samples_v.nui_samples_v_synsetid ON samples_v (synsetid) NOTUNIQUE
-CREATE INDEX samples_v.nui_samples_v_synsetid_seq ON samples_v (synsetid_seq) NOTUNIQUE
+CREATE CLASS samples_v EXTENDS V;
+CREATE PROPERTY samples_v.synsetid_seq LONG;
+CREATE PROPERTY samples_v.sample_seq LONG;
+CREATE PROPERTY samples_v.synsetid STRING;
+CREATE PROPERTY samples_v.sample STRING;
+CREATE INDEX samples_v.nui_samples_v_synsetid ON samples_v (synsetid) NOTUNIQUE;
+CREATE INDEX samples_v.nui_samples_v_synsetid_seq ON samples_v (synsetid_seq) NOTUNIQUE;
 CREATE INDEX samples_v.nui_samples_v_sample_seq ON samples_v (sample_seq) NOTUNIQUE
 
 --Отношения слово-значение
-CREATE CLASS has_senses_e EXTENDS E
-CREATE PROPERTY has_senses_e.posname STRING
-CREATE PROPERTY has_senses_e.origin STRING
-CREATE PROPERTY has_senses_e.wordtype STRING
-CREATE INDEX UNIQUE
+CREATE CLASS has_senses_e EXTENDS E;
+CREATE PROPERTY has_senses_e.posname STRING;
+CREATE PROPERTY has_senses_e.origin STRING;
+CREATE PROPERTY has_senses_e.wordtype STRING;
+CREATE PROPERTY has_senses_e.in LINK;
+CREATE PROPERTY has_senses_e.out LINK;
+CREATE INDEX has_senses_e.ui_has_senses_e ON has_senses_e (out, in) UNIQUE
 --Отношения между значениями - синонимы, антонимы и т.д.
-CREATE CLASS has_link_e EXTENDS E
-CREATE PROPERTY has_link_e.linktype STRING
-CREATE PROPERTY has_link_e.linkgroup STRING
-CREATE PROPERTY has_link_e.origin STRING
-CREATE INDEX UNIQUE
+CREATE CLASS has_link_e EXTENDS E;
+CREATE PROPERTY has_link_e.linktype STRING;
+CREATE PROPERTY has_link_e.linkgroup STRING;
+CREATE PROPERTY has_link_e.origin STRING;
+CREATE PROPERTY has_link_e.in LINK;
+CREATE PROPERTY has_link_e.out LINK;
+CREATE INDEX has_link_e.ui_has_link_e ON has_link_e (out, in) UNIQUE
 --Отношения значения-примеры использования
-CREATE CLASS has_samples_e EXTENDS E
-CREATE PROPERTY has_samples_e.origin STRING
-CREATE INDEX UNIQUE
+CREATE CLASS has_samples_e EXTENDS E;
+CREATE PROPERTY has_samples_e.origin STRING;
+CREATE PROPERTY has_samples_e.in LINK;
+CREATE PROPERTY has_samples_e.out LINK;
+CREATE INDEX has_samples_e.ui_has_samples_e ON has_samples_e (out, in) UNIQUE
 --Ссылки в words_v на себя, например слова в других регистрах (casedwords) и т.д.
-CREATE CLASS has_wordslink_e EXTENDS E
-CREATE PROPERTY has_wordslink_e.origin STRING
-CREATE PROPERTY has_wordslink_e.wordtype STRING
-CREATE INDEX UNIQUE
+CREATE CLASS has_wordslink_e EXTENDS E;
+CREATE PROPERTY has_wordslink_e.origin STRING;
+CREATE PROPERTY has_wordslink_e.wordtype STRING;
+CREATE PROPERTY has_wordslink_e.in LINK;
+CREATE PROPERTY has_wordslink_e.out LINK;
+CREATE INDEX has_wordslink_e.ui_has_wordslink_e ON has_wordslink_e (out, in) UNIQUE
 
 --INSERT INTO words_v FROM SELECT FROM words_doc
 
