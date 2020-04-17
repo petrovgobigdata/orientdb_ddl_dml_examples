9077fe3fce54ab205fff1052770600e73f5d16b9
petrovgobigdata <petrov.y.v@gmail.com>
2020-04-17 15:12:24 +0300

    Update orientdb__ddl_hierarchy.sql

--- orientdb_ddl_dml_examples/orientdb__ddl_hierarchy.sql
+++ orientdb_ddl_dml_examples/orientdb__ddl_hierarchy.sql
@@ -9,3 +9,6 @@ CREATE INDEX ypedu_person_v.ui_ypedu_person_v_person_id ON ypedu_person_v (perso
 CREATE CLASS has_family_link_e EXTENDS E;
 CREATE PROPERTY has_family_link_e.in LINK;
 CREATE PROPERTY has_family_link_e.out LINK
+
+$ORIENTDB_HOME/bin/oetl.sh /opt/oetl/sqlunet/scripts/ypedu_01.hierarchy.json
+$ORIENTDB_HOME/bin/oetl.sh /opt/oetl/sqlunet/scripts/ypedu_02.hierarchy.json
